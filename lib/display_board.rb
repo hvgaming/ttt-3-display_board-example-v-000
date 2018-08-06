591993ce1380e06ca0a696b37af670e5b6907040
hvgaming@yahoo.com <hvgaming@yahoo.com>
2018-08-06 19:52:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts "   |    |  "
   puts "-----------"
@@ -9,5 +9,5 @@ def display_board(board)
   puts "   |    |  "
 end
  
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
  
